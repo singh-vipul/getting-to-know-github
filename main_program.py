2b9d168dc6aa9e4dc41c352e83e22c6bf4d03b8d
Vipul Singh <singh06vipul@gmail.com>
2020-07-31 09:07:55 +0530

    Update main_program.py
    It tells if a number is prime or not.

--- main_program.py
+++ main_program.py
@@ -1,1 +1,10 @@
 # Count total prime numbers in a given range
+import primality_test
+
+
+input_number = input('Enter a number: ')
+
+if is_prime_number(input_number):
+  print(str(input_number) + ' is a Prime Number')
+else:
+  print(str(input_number) + ' is a Composite Number')
