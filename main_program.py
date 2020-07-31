91eba2f6466358d5ce9ea76d357d4d72e5f0a802
Vipul Singh <singh06vipul@gmail.com>
2020-07-31 09:38:37 +0530

    Improvise the process by caching and fix some bugs

--- main_program.py
+++ main_program.py
@@ -1,10 +1,28 @@
 # Count total prime numbers in a given range
-import primality_test
+from primality_test import is_prime_number
+import pickle
 
 
-input_number = input('Enter a number: ')
+def load_primes_from_cache():
+	prime_numbers = set()
+	try:
+		with open('cached.pickle', 'rb') as fp:
+			prime_numbers = pickle.load(fp)
+	except:
+		pass
+	return prime_numbers
 
-if is_prime_number(input_number):
+
+prime_numbers = load_primes_from_cache()
+
+input_number = int(input('Enter a number: '))
+
+if input_number in prime_numbers:
   print(str(input_number) + ' is a Prime Number')
+elif is_prime_number(input_number):
+  print(str(input_number) + ' is a Prime Number')
+  prime_numbers.add(input_number)
+  with open('cached.pickle', 'wb') as fp:
+    pickle.dump(prime_numbers, fp)
 else:
   print(str(input_number) + ' is a Composite Number')
